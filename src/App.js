2e8e5380d01b74f4d6aa1d4b28a9e6860d1bfdd4
sleep-ing <communionyung@gmail.com>
2022-04-19 20:45:28 +0900

    inital commit (move folder)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateButton from './components/CreateButton/Button';
+import MainPage from './Template/Mainpage';
+
+
+function App() {
+
+  
+  return (
+    <MainPage />
+  );
+}
+
+export default App;
