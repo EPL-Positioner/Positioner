ababd06ae7b2faaa1d177291d1000757d77aa436
sleep-ing <communionyung@gmail.com>
2022-04-19 20:36:46 +0900

    add header publishing

---
+++ positioner/src/components/CreateButton/Style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    width: 200px;
+    height: 40px;
+    background: #9EEAC5;
+    color: #F8F8F8;
+    font-size: 1.2rem;
+    text-align: center;
+    border: 0;
+    font-weight: 800;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #28EB8D;
+    }
+`
